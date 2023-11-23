65f2b01dff03e42e59c12d700d47b895f313d314
Devansh Kumar Singh <devansh9452@gmail.com>
2023-11-23 21:38:48 +0530

    Day 6 of 100DaysOfCode

---
+++ ar_3sum.java
@@ -0,0 +1,39 @@
+public class ar_3sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+
+        List<List<Integer>> finalList = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 1; i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                List<Integer> list = new ArrayList<>();
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    if (k != n - 1) {
+                        while (j < k && nums[k] == nums[k + 1]) {
+                            k--;
+                        }
+                    }
+
+                    finalList.add(list);
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return finalList;
+    }
+}
