16c33ce679e345b9db0422d04c9d9d9f2dc04f22
Devansh Kumar Singh <devansh9452@gmail.com>
2023-12-04 22:25:58 +0530

    Day 12 of 100DaysOfCode

---
+++ ar_largestSubarraywith0sum.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class ar_largestSubarraywith0sum {
+    public class Solution {
+        public static int getLongestZeroSumSubarrayLength(int[] A) {
+            // Write your code here.
+            HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
+
+            int maxi = 0;
+            int sum = 0;
+            int n = A.length;
+
+            for (int i = 0; i < n; i++) {
+
+                sum += A[i];
+
+                if (sum == 0) {
+                    maxi = i + 1;
+                } else {
+                    if (mpp.get(sum) != null) {
+
+                        maxi = Math.max(maxi, i - mpp.get(sum));
+                    } else {
+
+                        mpp.put(sum, i);
+                    }
+                }
+            }
+            return maxi;
+
+        }
+    }
+}
