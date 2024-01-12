cfe02c001113558ee4b3dbc03ea52cb0bc70af85
Devansh Kumar Singh <devansh9452@gmail.com>
2024-01-12 20:40:44 +0530

    Day 14 of 100DaysOfCode

---
+++ ar_bs_floorceil.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class ar_bs_floorceil {
+    static int findFloor(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int findCeil(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+}
