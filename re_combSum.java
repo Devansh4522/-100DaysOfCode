1aee51213139bd57526d4d245b20753484460638
Devansh Kumar Singh <devansh9452@gmail.com>
2024-01-14 21:09:37 +0530

    Day 42 of 100DaysOfCode

---
+++ re_combSum.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class re_combSum {
+    static List<List<Integer>> ans;
+
+    static void helper(int arr[], int idx, int target, List<Integer> a) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(a));
+            return;
+        }
+        if (idx >= arr.length || target < 0)
+            return;
+
+        a.add(arr[idx]);
+        helper(arr, idx, target - arr[idx], a);
+        a.remove(a.size() - 1);
+        helper(arr, idx + 1, target, a);
+    }
+
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        ans = new ArrayList<>();
+        helper(arr, 0, target, new ArrayList<>());
+        return ans;
+    }
+}
