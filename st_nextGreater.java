45ea7d8c7210d137e92a99252d52cd900d361340
Devansh Kumar Singh <devansh9452@gmail.com>
2024-01-30 21:57:12 +0530

    Day 51 of 100DaysOfCode

---
+++ st_nextGreater.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class st_nextGreater {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[nums.length];
+        for (int i = 2 * n - 1; i >= 0; i--) {
+
+            while ((!st.isEmpty()) && ((st.peek()) <= nums[i % n])) {
+                st.pop();
+            }
+            if (i < n) {
+                if (!st.isEmpty()) {
+                    ans[i] = st.peek();
+                } else {
+                    ans[i] = -1;
+                }
+            }
+            st.push(nums[i % n]);
+
+        }
+        return ans;
+    }
+}
