8b537a15e4a3c0c898a352b47c74fb863e80678a
Devansh Kumar Singh <devansh9452@gmail.com>
2024-03-24 21:45:59 +0530

    Day 78 of 100DaysOfCode

---
+++ re_wordCount.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class re_wordCount {
+    class Solution {
+        private Map<String, Boolean> dp;
+
+        public boolean solve(String s, Set<String> m) {
+            if (s.length() == 0) {
+                return true;
+            }
+            if (dp.containsKey(s))
+                return dp.get(s);
+            for (int i = 0; i < s.length(); i++) {
+                String r = s.substring(0, i + 1);
+                if (m.contains(r)) {
+                    if (solve(s.substring(i + 1), m)) {
+                        dp.put(s, true);
+                        return true;
+                    }
+                }
+            }
+            dp.put(s, false);
+            return false;
+        }
+
+        public boolean wordBreak(String s, List<String> wordDict) {
+            Set<String> m = new HashSet<>(wordDict);
+            dp = new HashMap<>();
+            return solve(s, m);
+        }
+    }
+}
