2d627899aa0d429758a6f535d00b3f2c2be55301
Devansh Kumar Singh <devansh9452@gmail.com>
2024-04-13 21:34:03 +0530

    Day 90 of 100DaysOfCode

---
+++ str_minAddParBal.java
@@ -0,0 +1,18 @@
+public class str_minAddParBal {
+    class Solution {
+        public int minAddToMakeValid(String s) {
+            int left = 0, right = 0;
+
+            for (int i = 0; i < s.length(); ++i) {
+                if (s.charAt(i) == '(')
+                    left++;
+                else if (left > 0)
+                    left--;
+                else
+                    right++;
+            }
+
+            return left + right;
+        }
+    }
+}
